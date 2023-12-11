5a9bc1a330fb94d9f855e710fa6ab9078bbc955c
mikemospan <28594918+TheCodingMike@users.noreply.github.com>
2023-12-11 19:06:08 +1100

    Fixed Knight Movement

--- Pieces/Knight.java
+++ Pieces/Knight.java
@@ -3,6 +3,6 @@ package Pieces;
 public class Knight extends Piece {
     @Override
     public boolean canMove(int x, int y) {
-        return Math.abs(getX() - x) == 2 && Math.abs(getY() - y) == 1;
+        return Math.abs(getX() - x) == 1 && Math.abs(getY() - y) == 2;
     }
 }

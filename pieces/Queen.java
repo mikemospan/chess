0ce24e74c93caddf12387e8f096650203fbc56a4
sawlani63 <reachchiragsawlani@gmail.com>
2023-12-18 23:17:30 +1100

    Positioned white pieces on board
    Created constructors for each piece and setting its initial column and row which is used by paintComponent in the Piece class to position the image

--- pieces/Queen.java
+++ pieces/Queen.java
@@ -1,12 +1,15 @@
 package pieces;
 
 public class Queen extends Piece {
+    public Queen (int col, int row) {
+        super(col, row);
+    }
     @Override
     public boolean canMove(int col, int row) {
-        return new Bishop().canMove(col, row) || new Rook().canMove(col, row);
+        return new Bishop(col, row).canMove(col, row) || new Rook(col, row).canMove(col, row);
     }
 
     public String getURL() {
-        return "/pieces/images/white_king.png";
+        return "/pieces/images/white_queen.png";
     }
 }
--- pieces/Rook.java
+++ pieces/Rook.java
@@ -1,12 +1,15 @@
 package pieces;
 
 public class Rook extends Piece {
+    public Rook(int col, int row) {
+        super(col, row);
+    }
     @Override
     public boolean canMove(int col, int row) {
         return (getCol() == col && getRow() != row) || (getRow() == row && getCol() != col);
     }
 
     public String getURL() {
-        return "/pieces/images/white_king.png";
+        return "/pieces/images/white_rook.png";
     }
 }
